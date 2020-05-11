11a38d6ec72e82d1013e2670a137e0392ad37768
thewhitewolf2411 <39027423+thewhitewolf2411@users.noreply.github.com>
2020-05-11 22:01:25 +0200

    axios

--- resources/js/components/AddPizza.jsx
+++ resources/js/components/AddPizza.jsx
@@ -42,6 +42,7 @@ export default class AddPizza extends Component{
             .then(res=>{
                 console.log(res);
             })
+            .catch((error) => console.log(error));
     }
 
     render(){
